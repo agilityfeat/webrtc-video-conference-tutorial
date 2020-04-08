81ae6eca776680f283636245d13de31d5296112e
Hector Zelaya <inge.zelaya@gmail.com>
2020-04-08 15:58:30 -0600

    added participants names

--- public/client.js
+++ public/client.js
@@ -6,6 +6,8 @@ const inputRoomNumber = document.getElementById("roomNumber");
 const btnJoinBroadcaster = document.getElementById("joinBroadcaster");
 const btnJoinViewer = document.getElementById("joinViewer");
 const videoElement = document.querySelector("video");
+const broadcasterName = document.getElementById("broadcasterName");
+const viewers = document.getElementById("viewers");
 
 // variables
 let user;
@@ -18,7 +20,7 @@ const iceServers = {
     { urls: "stun:stun.l.google.com:19302" },
   ],
 };
-const streamConstraints = { audio: false, video: true };
+const streamConstraints = { audio: false, video: { height: 480 } };
 
 // Let's do this 💪
 var socket = io();
@@ -28,73 +30,80 @@ btnJoinBroadcaster.onclick = function () {
     alert("Please type a room number and a name");
   } else {
     user = {
-        room: inputRoomNumber.value,
-        name: inputName.value
+      room: inputRoomNumber.value,
+      name: inputName.value,
     };
-    
+
     divSelectRoom.style = "display: none;";
     divConsultingRoom.style = "display: block;";
+    broadcasterName.innerText = user.name + " is broadcasting...";
 
     navigator.mediaDevices
-    .getUserMedia(streamConstraints)
-    .then(function (stream) {
-      videoElement.srcObject = stream;
-      socket.emit("register as broadcaster", user.room);
-    })
-    .catch(function (err) {
-      console.log("An error ocurred when accessing media devices", err);
-    });
+      .getUserMedia(streamConstraints)
+      .then(function (stream) {
+        videoElement.srcObject = stream;
+        socket.emit("register as broadcaster", user.room);
+      })
+      .catch(function (err) {
+        console.log("An error ocurred when accessing media devices", err);
+      });
   }
 };
 
 btnJoinViewer.onclick = function () {
-    if (inputRoomNumber.value === "" || inputName.value === "") {
-        alert("Please type a room number and a name");
-      } else {
-        user = {
-            room: inputRoomNumber.value,
-            name: inputName.value
-        };
-        
-        divSelectRoom.style = "display: none;";
-        divConsultingRoom.style = "display: block;";
-    
-        socket.emit("register as viewer", user);
-      }
-}
+  if (inputRoomNumber.value === "" || inputName.value === "") {
+    alert("Please type a room number and a name");
+  } else {
+    user = {
+      room: inputRoomNumber.value,
+      name: inputName.value,
+    };
+
+    divSelectRoom.style = "display: none;";
+    divConsultingRoom.style = "display: block;";
+
+    socket.emit("register as viewer", user);
+  }
+};
 
 // message handlers
 socket.on("new viewer", function (viewer) {
-    rtcPeerConnections[viewer.id] = new RTCPeerConnection(iceServers);
+  rtcPeerConnections[viewer.id] = new RTCPeerConnection(iceServers);
 
-    const stream = videoElement.srcObject
-    stream.getTracks().forEach(track => rtcPeerConnections[viewer.id].addTrack(track, stream));
+  const stream = videoElement.srcObject;
+  stream
+    .getTracks()
+    .forEach((track) => rtcPeerConnections[viewer.id].addTrack(track, stream));
 
-    rtcPeerConnections[viewer.id].onicecandidate = event => {
-        if (event.candidate) {
-            console.log("sending ice candidate");
-            socket.emit("candidate", viewer.id, {
-              type: "candidate",
-              label: event.candidate.sdpMLineIndex,
-              id: event.candidate.sdpMid,
-              candidate: event.candidate.candidate
-            });
-          }
-    };
-    
-    rtcPeerConnections[viewer.id]
-      .createOffer()
-      .then((sessionDescription) => {
-        rtcPeerConnections[viewer.id].setLocalDescription(sessionDescription);
-        socket.emit("offer", viewer.id, {
-          type: "offer",
-          sdp: sessionDescription,
-          broadcaster: user
-        });
-      })
-      .catch((error) => {
-        console.log(error);
+  rtcPeerConnections[viewer.id].onicecandidate = (event) => {
+    if (event.candidate) {
+      console.log("sending ice candidate");
+      socket.emit("candidate", viewer.id, {
+        type: "candidate",
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate,
       });
+    }
+  };
+
+  rtcPeerConnections[viewer.id]
+    .createOffer()
+    .then((sessionDescription) => {
+      rtcPeerConnections[viewer.id].setLocalDescription(sessionDescription);
+      socket.emit("offer", viewer.id, {
+        type: "offer",
+        sdp: sessionDescription,
+        broadcaster: user,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  let li = document.createElement("li");
+  li.innerText = viewer.name + " has joined";
+  viewers.appendChild(li);
 });
 
 socket.on("candidate", function (id, event) {
@@ -106,38 +115,44 @@ socket.on("candidate", function (id, event) {
 });
 
 socket.on("offer", function (broadcaster, sdp) {
-    rtcPeerConnections[broadcaster.id] = new RTCPeerConnection(iceServers);
+  broadcasterName.innerText = broadcaster.name + "is broadcasting...";
 
-    rtcPeerConnections[broadcaster.id].setRemoteDescription(sdp);
+  rtcPeerConnections[broadcaster.id] = new RTCPeerConnection(iceServers);
 
-    rtcPeerConnections[broadcaster.id]
-        .createAnswer()
-        .then(sessionDescription => {
-            rtcPeerConnections[broadcaster.id].setLocalDescription(sessionDescription);
-            socket.emit("answer", {
-                type: "answer",
-                sdp: sessionDescription,
-                room: user.room
-            })
-        })
+  rtcPeerConnections[broadcaster.id].setRemoteDescription(sdp);
 
-    rtcPeerConnections[broadcaster.id].ontrack = event => {
-        videoElement.srcObject = event.streams[0];
+  rtcPeerConnections[broadcaster.id]
+    .createAnswer()
+    .then((sessionDescription) => {
+      rtcPeerConnections[broadcaster.id].setLocalDescription(
+        sessionDescription
+      );
+      socket.emit("answer", {
+        type: "answer",
+        sdp: sessionDescription,
+        room: user.room,
+      });
+    });
+
+  rtcPeerConnections[broadcaster.id].ontrack = (event) => {
+    videoElement.srcObject = event.streams[0];
+  };
+
+  rtcPeerConnections[broadcaster.id].onicecandidate = (event) => {
+    if (event.candidate) {
+      console.log("sending ice candidate");
+      socket.emit("candidate", broadcaster.id, {
+        type: "candidate",
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate,
+      });
     }
-
-    rtcPeerConnections[broadcaster.id].onicecandidate = event => {
-        if (event.candidate) {
-            console.log("sending ice candidate");
-            socket.emit("candidate", broadcaster.id, {
-              type: "candidate",
-              label: event.candidate.sdpMLineIndex,
-              id: event.candidate.sdpMid,
-              candidate: event.candidate.candidate
-            });
-          }
-    };
+  };
 });
 
 socket.on("answer", function (viewerId, event) {
-  rtcPeerConnections[viewerId].setRemoteDescription(new RTCSessionDescription(event));
+  rtcPeerConnections[viewerId].setRemoteDescription(
+    new RTCSessionDescription(event)
+  );
 });
